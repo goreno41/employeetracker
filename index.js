e01ff0e178c7c9b633e3827abd2bdf216bf39a11
Josh Renaud <josh.renaud41@gmail.com>
2021-07-17 13:39:28 -0500

    added functions for add role and update employee role

--- index.js
+++ index.js
@@ -4,6 +4,34 @@ const mysql = require("mysql");
 const {config} = require("./creds/creds")
 const connection = mysql.createConnection(config);
 
+//.. Role options function within add employee
+
+var rolesList = [];
+const roleOptions = () => {
+  connection.query("SELECT * FROM role", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      rolesList.push(res[i].name);
+    }
+
+  })
+  return rolesList;
+}
+
+//.. Select manager function within add employee
+
+var managersList = [];
+const selectManager = () => {
+  connection.query("SELECT first_name, last_name FROM employee WHERE manager_id IS NULL", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      managersList.push(res[i].first_name);
+    }
+
+  })
+  return managersList;
+}
+
 //.. Add Employee function
 
 const addEmployee = () => { 
@@ -31,7 +59,7 @@ const addEmployee = () => {
             name: "manager"
         }
     ]).then(function (val) {
-      var roleId = selectRole().indexOf(val.role) + 1
+      var roleId = roleOptions().indexOf(val.role) + 1
       var managerId = selectManager().indexOf(val.manager) + 1
       connection.query("INSERT INTO employee SET ?", 
       {
@@ -43,70 +71,123 @@ const addEmployee = () => {
       }, function(err){
           if (err) throw err
           console.table(val)
-          startPrompt()
+          mainMenu()
       })
 
   })
 }
 
-//.. Role options function within add employee
-
-var rolesList = [];
-function roleOptions() {
-  connection.query("SELECT * FROM role", function(err, res) {
-    if (err) throw err
-    for (var i = 0; i < res.length; i++) {
-      rolesList.push(res[i].name);
-    }
-
-  })
-  return rolesList;
-}
-
-//.. Select manager function within add employee
-
-var managersList = [];
-function selectManager() {
-  connection.query("SELECT first_name, last_name FROM employee WHERE manager_id IS NULL", function(err, res) {
-    if (err) throw err
-    for (var i = 0; i < res.length; i++) {
-      managersList.push(res[i].first_name);
-    }
-
-  })
-  return managersList;
-}
 
 //.. Update Employee Info function
 
-updateInfo();
+const updateRole = () => {
+    connection.query("SELECT employee.last_name, role.name FROM employee JOIN role ON employee.role_id = role.id;", function(err, res) {
+     if (err) throw err
+     console.log(res)
+    inquirer.prompt([
+          {
+              type: "list",
+              message: "What is the employee's last name?",
+              choices: function() {
+                  var lastName = [];
+                  for (var i = 0; i < res.length; i++) {
+                      lastName.push(res[i].last_name);
+                    }
+                    return lastName;
+                },
+                name: "lastName"
+          },
+          {
+              type: "list",
+              message: "What is the employee's new title?",
+              choices: roleOptions(),
+              name: "role"
+          },
+      ]).then(function(val) {
+        var roleId = roleOptions().indexOf(val.role) + 1
+        connection.query("UPDATE employee SET WHERE ?", 
+        {
+          last_name: val.lastName
+           
+        }, 
+        {
+          role_id: roleId
+           
+        }, 
+        function(err){
+            if (err) throw err
+            console.table(val)
+            mainMenu()
+        })
+  
+    });
+  });
+
+  }
 
 //.. Add role function
 
-addRole();
+const addRole = () => {
+    connection.query("SELECT role.name AS name, role.salary AS salary FROM role",   function(err, res) {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is the name of the new role?",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "What is the salary for the new role?",
+                name: "salary"
+    
+            } 
+        ]).then(function(res) {
+            connection.query(
+                "INSERT INTO role SET ?",
+                {
+                  name: res.name,
+                  salary: res.salary,
+                },
+                function(err) {
+                    if (err) throw err
+                    console.table(res);
+                    mainMenu();
+                }
+            )
+    
+        });
+      });
+
+}
 
 //.. Add department function
 
-addDepartment();
+const addDepartment = () => {
+
+}
 
 //.. View all employees function
 
-viewAllEmployees();
+const viewAllEmployees = () => {
+
+}
 
 //.. View employess by role function
 
-viewAllRoles();
+const viewAllRoles = () => {
+
+}
 
 //.. View employees by department function
 
-viewAllDepartments();
+const viewAllDepartments = () => {
 
-
+}
 
 
 //.. Main menu to give user options
 
-function startPrompt() {
+const mainMenu = () => {
     inquirer.prompt([
     {
     type: "list",
@@ -114,9 +195,9 @@ function startPrompt() {
     name: "action",
     choices: [
             "Add Employee",
-            "Update Employee Info",
-            "Add Role",
-            "Add Department",
+            "Change Employee Role",
+            "Add New Role",
+            "Add New Department",
             "View All Employees", 
             "View All Employees By Roles",
             "View all Employees By Deparments"
@@ -127,13 +208,13 @@ function startPrompt() {
             case "Add Employee":
                     addEmployee();
                 break;
-            case "Update Employee Info":
-                    updateInfo();
+            case "Change Employee Role":
+                    updateRole();
                 break;
-            case "Add Role":
+            case "Add New Role":
                     addRole();
                 break;
-            case "Add Department":
+            case "Add New Department":
                     addDepartment();
                 break;
             case "View All Employees":
@@ -154,4 +235,4 @@ function startPrompt() {
 
 
 
-startPrompt();
+mainMenu();
